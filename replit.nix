558dbae81e056b41e8d7100f6eb5d51a8c0d1196
Dražen Popović <bladyjoker@gmail.com>
2022-10-14 08:18:09 +0000

    Added Replit Haskell support and a Haskell sketch for the Source DSL

--- replit.nix
+++ replit.nix
@@ -1,8 +1,15 @@
 { pkgs }: {
     deps = [
+        pkgs.haskellPackages.ghc
         pkgs.cowsay
         pkgs.dhall
         pkgs.dhall-lsp-server
         pkgs.dhall-json
+
+        (pkgs.haskellPackages.ghcWithPackages (pkgs: [
+          pkgs.text
+        ]))
+        pkgs.haskell-language-server      
     ];
+  
 }
---
+++ repls/draz-lambuffs-source-dsl.hs
@@ -0,0 +1,114 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module LambdaBuffers.Source where
+
+import Data.Foldable
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Text (Text)
+
+type VarName = Text
+
+type TyConName = Text
+
+newtype TKind = TKind [VarName]
+
+data TypeRef = Local TyConName | Foreign ModuleName TyConName
+
+data Ty = TyVar VarName | TCon TKind | TApp Ty Ty | TyRef TypeRef
+
+data TyDef = TyDef
+  { tyDefName :: TyConName,
+    tyDefKind :: TKind,
+    tyDefBody :: TyBody
+  }
+
+data FieldName = Text
+
+data Product = Record (Map FieldName Ty) | Product [Ty] | Empty
+
+type ConstrName = Text
+
+data TyBody = Opaque | Sum (Map ConstrName Product)
+
+type ModuleName = Text
+
+data Module = Module
+  { moduleName :: ModuleName,
+    moduleDefs :: [TyDef],
+    moduleInstances :: [InstanceClause]
+  }
+
+data InstanceClause = InstanceClause
+  { icClassName :: Text,
+    icHead :: Ty,
+    icBody :: [Constraint]
+  }
+
+data Constraint = Constraint
+  { cClassName :: Text,
+    cArguments :: [Ty]
+  }
+
+opaque tyConName args = TyDef tyConName (TKind args) Opaque
+
+dt tyConName args constructors = TyDef tyConName (TKind args) (Sum $ Map.fromList [(cname, prod) | (cname, prod) <- constructors])
+
+var = TyVar
+
+vars vnames = Product $ TyVar <$> vnames
+
+nullary cn = (cn, Empty)
+
+app = TApp
+
+ty = TyRef . Local
+
+prod = Product
+
+recrd = Record
+
+fty m = TyRef . Foreign (moduleName m)
+
+ftyMyMod = fty myModule
+
+jsonInstance = InstanceClause "JSON"
+
+jsonConstr = Constraint "JSON"
+
+-- | User defs
+myModule =
+  Module
+    { moduleName = "MyModule",
+      moduleDefs =
+        [ opaque "Integer" [],
+          opaque "Set" ["a"],
+          opaque "Map" ["k", "v"],
+          dt "Maybe" ["a"] [("Just", prod [var "a"]), nullary "Nothing"],
+          dt
+            "MyType"
+            ["a", "b"]
+            [("Foo", prod [ty "Maybe" `app` ty "Map" `app` var "a" `app` (ty "Set" `app` var "b")])]
+        ],
+      moduleInstances =
+        [ jsonInstance (ty "Maybe" `app` var "a") [jsonConstr [var "a"]]
+        ]
+    }
+
+anotherModule =
+  Module
+    { moduleName = "AnotherModule",
+      moduleDefs =
+        [ dt
+            "AnotherType"
+            ["a", "b"]
+            [("Bar", prod [ftyMyMod "Maybe" `app` ftyMyMod "Map" `app` var "a" `app` (ftyMyMod "Set" `app` var "b")])]
+        ],
+      moduleInstances =
+        [ jsonInstance (ftyMyMod "Maybe" `app` var "a") [jsonConstr [var "a"]]
+        ]
+    }
+
+main = do
+  putStrLn "Hello"
+  putStrLn "World"
