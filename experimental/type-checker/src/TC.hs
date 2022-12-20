05e92fdbc9252d31ca2bee7d646e0537314849ee
Vlad P. Luchian <cstmlcodes@gmail.com>
2022-12-20 17:05:00 +0000

    update: fix unification + tidy up

--- experimental/type-checker/src/TC.hs
+++ experimental/type-checker/src/TC.hs
@@ -1,24 +1,16 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-
 module TC where
 
-import Control.Monad.Trans.Class
 import Control.Monad.Trans.Reader (ReaderT, runReaderT)
 import Control.Monad.Trans.State (StateT, evalStateT)
 import Control.Monad.Trans.Writer (WriterT (runWriterT))
+import Data.Bifunctor
 
 import Control.Monad.Except
 import Control.Monad.Reader.Class
 import Control.Monad.State.Class
 import Control.Monad.Writer.Class
 
-import Control.Monad.Identity (Identity (runIdentity))
-import Data.Map qualified as Map
+import Prettyprinter
 
 type Atom = String
 infixr 8 :->:
@@ -27,35 +19,53 @@ data Kind
   = Kind
   | Kind :->: Kind
   | KVar String
-  deriving (Eq)
+  deriving (Eq, Show)
 
-instance Show Kind where
-  show = \case
-    Kind -> "*"
-    x :->: y -> show x <> " -> " <> show y
-    KVar a -> a
+instance Pretty Kind where
+  pretty = \case
+    Kind -> pretty "*"
+    x :->: y -> pretty x <+> pretty "→" <+> pretty y
+    KVar a -> pretty a
 
 data Type
   = Var Atom
   | App Type Type
   | Abs String Type
-  deriving (Eq)
+  deriving (Eq, Show)
 
-instance Show Type where
-  show = \case
-    Var a -> a
-    App t1 t2 -> show' t1 <> " " <> show' t2
-    Abs a t1 -> "λ" <> a <> "." <> show t1
+instance Pretty Type where
+  pretty = \case
+    Var a -> pretty a
+    App t1 t2 -> show' t1 <> pretty " " <> show' t2
+    Abs a t1 -> pretty "λ" <> pretty a <> pretty "." <> pretty t1
     where
       show' = \case
-        Var a -> a
-        App t1 t2 -> ll $ show' t1 <> " " <> show' t2
-        Abs a t1 -> ll $ "λ" <> a <> "." <> show' t1
-      ll t = "(" <> t <> ")"
+        Var a -> pretty a
+        App t1 t2 -> parens $ show' t1 <+> show' t2
+        Abs a t1 -> parens $ pretty "λ" <> pretty a <> pretty "." <> show' t1
 
-type Context = [(Atom, Kind)]
+data Context = Context
+  { context :: [(Atom, Kind)]
+  , addContext :: [(Atom, Kind)]
+  }
+  deriving (Show, Eq)
 
-type Judgement = (Context, Type, Kind)
+instance Pretty Context where
+  pretty c = case addContext c of
+    [] -> pretty "Γ"
+    ctx -> pretty "Γ" <+> pretty "∪" <+> braces (setPretty ctx)
+    where
+      setPretty = hsep . punctuate comma . fmap (\(v, t) -> pretty v <> pretty ":" <+> pretty t)
+
+-- | Utility to unify the two.
+getAllContext :: Context -> [(Atom, Kind)]
+getAllContext c = context c <> addContext c
+
+newtype Judgement = Judgement {getJudgement :: (Context, Type, Kind)}
+  deriving (Show, Eq)
+
+instance Pretty Judgement where
+  pretty (Judgement (c, t, k)) = pretty c <> pretty " ⊢ " <> pretty t <+> pretty ":" <+> pretty k
 
 data Derivation
   = Axiom Judgement
@@ -63,11 +73,32 @@ data Derivation
   | Application Judgement Derivation Derivation
   deriving (Show, Eq)
 
+tterm = App (App (Abs "b" (App (Var "b") (Var "Int"))) (Var "Int")) (App (Var "Int") (Var "Int"))
+tterm2 = Var "Int"
+tterm3 = Abs "y" $ Abs "x" $ App (Var "x") (Var "Int")
+tterm4 = App (Abs "x" $ App (Var "x") (Var "Int")) (Var "Int")
+
+instance Pretty Derivation where
+  pretty x = case x of
+    Axiom j -> hang 2 $ pretty j
+    Abstraction j d -> dNest j [d]
+    Application j d1 d2 -> dNest j [d1, d2]
+    where
+      dNest j ds = pretty j <> line <> hang 2 (encloseSep (lbracket <> space) rbracket (space <> pretty "∧" <> space) (pretty <$> ds))
+
 type DError = String
 
-type Constraint = (Kind, Kind)
+newtype Constraint = Constraint (Kind, Kind)
+  deriving (Show, Eq)
 
-type Substitution = (Atom, Kind)
+instance Pretty Constraint where
+  pretty (Constraint (t1, t2)) = pretty t1 <+> pretty "=" <+> pretty t2
+
+newtype Substitution = Substitution {getSubstitution :: (Atom, Kind)}
+  deriving (Show, Eq)
+
+instance Pretty Substitution where
+  pretty (Substitution (a, k)) = pretty a <+> pretty "↦" <+> pretty k
 
 newtype DerivationContext = DC
   { freshVarStream :: [Atom]
@@ -90,39 +121,39 @@ runDerive' t = either error id $ runExcept $ runWriterT $ evalStateT (runReaderT
 -- | Creates the derivation
 derive :: Type -> Derive Derivation
 derive x = do
-  ctx <- ask
+  c <- ask
   case x of
     Var at -> do
       v <- getBinding at
-      pure $ Axiom (ctx, x, v)
+      pure $ Axiom $ Judgement (c, x, v)
     App t1 t2 -> do
       d1 <- derive t1
       d2 <- derive t2
       let ty1 = getKind d1
           ty2 = getKind d2
       v <- KVar <$> fresh
-      tell [(ty1, ty2 :->: v)]
-      pure $ Application (ctx, x, v) d1 d2
+      tell [Constraint (ty1, ty2 :->: v)]
+      pure $ Application (Judgement (c, x, v)) d1 d2
     Abs v t -> do
       newTy <- KVar <$> fresh
-      d <- local ((v, newTy) :) (derive t)
+      d <- local (\(Context c addC) -> Context c $ (v, newTy) : addC) (derive t)
       let ty = getKind d
       freshT <- KVar <$> fresh
-      tell [(freshT, newTy :->: ty)]
-      pure $ Abstraction (ctx, x, freshT) d
+      tell [Constraint (freshT, newTy :->: ty)]
+      pure $ Abstraction (Judgement (c, x, freshT)) d
 
 getBinding :: Atom -> Derive Kind
 getBinding t = do
-  ctx <- ask
+  ctx <- asks getAllContext
   case t `lookup` ctx of
     Just x -> pure x
-    Nothing -> throwError $ "Unbound term:" <> t
+    Nothing -> throwError $ "Unbound term:" <> show (pretty t)
 
 getKind :: Derivation -> Kind
 getKind = \case
-  Axiom (_, _, k) -> k
-  Abstraction (_, _, k) _ -> k
-  Application (_, _, k) _ _ -> k
+  Axiom (Judgement (_, _, k)) -> k
+  Abstraction (Judgement (_, _, k)) _ -> k
+  Application (Judgement (_, _, k)) _ _ -> k
 
 type UErr = String
 type Unifier a = Except UErr a
@@ -133,82 +164,108 @@ getVariables = \case
   x :->: y -> getVariables x <> getVariables y
   KVar x -> [x]
 
-unify :: Constraint -> Unifier [Substitution]
-unify (l, r) = case l of
+unify :: [Constraint] -> Unifier [Substitution]
+unify [] = pure []
+unify ((Constraint (l, r)) : xs) = case l of
   Kind -> case r of
-    Kind -> pure mempty
+    Kind -> unify xs
     (_ :->: _) -> nope l r
-    KVar v -> pure [(v, Kind)]
+    KVar v -> (Substitution (v, Kind) :) <$> unify xs
   x :->: y -> case r of
     Kind -> nope l r
     KVar v ->
       if v `appearsIn` l
         then appearsErr l v
-        else pure [(v, l)]
-    m :->: n -> (<>) <$> unify (x, m) <*> unify (y, n)
+        else
+          let sub = Substitution (v, l)
+           in (sub :) <$> unify (sub `substituteIn` xs)
+    m :->: n ->
+      let c1 = Constraint (x, m)
+          c2 = Constraint (y, n)
+       in unify (c1 : c2 : xs)
   KVar a -> case r of
-    KVar b -> pure [(a, r)]
-    _ -> unify (r, l)
+    KVar b ->
+      let sub = Substitution (a, r)
+       in (sub :) <$> unify (sub `substituteIn` xs)
+    _ -> unify $ Constraint (r, l) : xs
   where
-    nope l r = throwError $ unlines ["Cannot unify: ", show l <> " with " <> show r]
+    nope l r = throwError $ unlines ["Cannot unify: ", show (pretty l) <> " with " <> show (pretty r)]
     appearsErr v r =
       throwError $
         unlines
           [ "Cannot unify: "
-          , show v <> " with " <> show r
-          , "because: " <> show v <> " appears in " <> show r
+          , show (hang 4 $ pretty $ show (pretty v) <> " with " <> show (pretty r))
+          , "because: " <> show (pretty v) <> " appears in " <> show (pretty r)
           ]
     appearsIn a l = a `elem` getVariables l
 
-unify2 :: [Substitution] -> Unifier [Substitution]
-unify2 subs = case subs of
-  [] -> pure []
-  (at, ty) : xs -> case lookup at xs of
-    Just ty2 -> (<>) <$> unify2 xs <*> unify (ty, ty2)
-    Nothing -> ((at, ty) :) <$> unify2 xs
+    substituteIn s [] = []
+    substituteIn s ((Constraint (l, r)) : xs) = Constraint (applySubstitution s l, applySubstitution s r) : substituteIn s xs
+
+applySubstitution :: Substitution -> Kind -> Kind
+applySubstitution s@(Substitution (a, t)) = \case
+  Kind -> Kind
+  l :->: r -> applySubstitution s l :->: applySubstitution s r
+  KVar v -> if v == a then t else KVar v
 
 runUnify :: [Constraint] -> Either UErr [Substitution]
-runUnify constraints = runExcept $ do
-  subs <- mconcat <$> mapM unify constraints
-  unify2 subs
+runUnify constraints = runExcept $ unify constraints
 
 substitute :: Substitution -> Derivation -> Derivation
 substitute s d = case d of
-  Axiom (ctx, t, k) -> Axiom (ctx, t, applySubstitution s k)
+  Axiom j -> Axiom (applySubsToJudgement s j)
   Abstraction j dc -> Abstraction (applySubsToJudgement s j) (substitute s dc)
   Application j d1 d2 -> Application (applySubsToJudgement s j) (substitute s d1) (substitute s d2)
   where
-    applySubsToJudgement s (ctx, t, k) = (ctx, t, applySubstitution s k)
-    applySubstitution subs@(a, nt) = \case
+    applySubsToJudgement sub (Judgement (ctx, t, k)) = Judgement (applySubstitutionCtx s ctx, t, applySubstitution sub k)
+
+    applySubstitution sub@(Substitution (a, nt)) = \case
       Kind -> Kind
-      l :->: r -> applySubstitution subs l :->: applySubstitution subs r
+      l :->: r -> applySubstitution sub l :->: applySubstitution sub r
       b@(KVar ab) -> if a == ab then nt else b
 
--- | Testing function
+    applySubstitutionCtx s c@(Context ctx addCtx) = case addCtx of
+      [] -> c
+      xs -> Context ctx $ second (applySubstitution s) <$> xs
+
+----------------------------------------------------------------------------------
+--  Testing functions
 getType :: Type -> IO ()
 getType t = do
   let (d, c) = runDerive' t
-  print (d, c)
+  print $ pretty (d, c)
+  putStrLn ""
   let s = either error id $ runUnify c
-  print s
-  let res = go (go d s) s
-  print res
-  putStrLn $ show t <> ":" <> show (getKind res)
+  print $ pretty s
+  putStrLn ""
+  let res = go d s
+  print $ pretty res
+  putStrLn ""
+  putStrLn $ show (pretty t) <> ":" <> (show . pretty $ getKind res)
   where
     go = foldl (flip substitute)
 
+testDerivation :: Type -> IO ()
+testDerivation t = do
+  let (d, c) = runDerive' t
+  print $ pretty d
+
 atoms :: [Atom]
-atoms = ['a' ..] >>= \x -> ['1' ..] >>= \y -> pure [x, y]
+atoms = ['1' ..] >>= \y -> ['a' .. 'z'] >>= \x -> pure [x, y]
 
 defContext :: Context
 defContext =
-  [ ("Either", Kind :->: Kind :->: Kind)
-  , ("Maybe", Kind :->: Kind)
-  , ("(,)", Kind :->: Kind :->: Kind)
-  , ("Int", Kind)
-  , ("Bool", Kind)
-  , ("Map", Kind :->: Kind :->: Kind)
-  ]
+  Context
+    { context =
+        [ ("Either", Kind :->: Kind :->: Kind)
+        , ("Maybe", Kind :->: Kind)
+        , ("(,)", Kind :->: Kind :->: Kind)
+        , ("Int", Kind)
+        , ("Bool", Kind)
+        , ("Map", Kind :->: Kind :->: Kind)
+        ]
+    , addContext = []
+    }
 
 fresh :: Derive Atom
 fresh = do
