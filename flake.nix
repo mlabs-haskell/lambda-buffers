e3420f57ab5f1aa9203a96cf9db43c1f710cfb40
Drazen Popovic <bladyjoker@gmail.com>
2022-12-22 12:11:05 +0100

    Added protobufs.nix to the repo

--- flake.nix
+++ flake.nix
@@ -5,72 +5,76 @@
     nixpkgs.follows = "haskell-nix/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    protobufs-nix.url = "github:mlabs-haskell/protobufs.nix";
   };
 
-  outputs = { self, nixpkgs, haskell-nix, flake-utils, pre-commit-hooks }: flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-    let
-      inherit self haskell-nix;
+  outputs = { self, nixpkgs, haskell-nix, flake-utils, pre-commit-hooks, protobufs-nix }: flake-utils.lib.eachSystem [ "x86_64-linux" ]
+    (system:
+      let
+        inherit self haskell-nix;
 
-      pkgs = import nixpkgs {
-        inherit system;
-      };
+        pkgs = import nixpkgs {
+          inherit system;
+        };
 
-      # pre-commit-hooks.nix
-      fourmolu = pkgs.haskell.packages.ghc924.fourmolu;
+        # pre-commit-hooks.nix
+        fourmolu = pkgs.haskell.packages.ghc924.fourmolu;
 
-      pre-commit-check = pre-commit-hooks.lib.${system}.run (import ./pre-commit-check.nix {
-        inherit pkgs fourmolu;
-      });
+        pre-commit-check = pre-commit-hooks.lib.${system}.run (import ./pre-commit-check.nix {
+          inherit pkgs fourmolu;
+        });
 
-      preCommitTools = pre-commit-hooks.outputs.packages.${system};
+        preCommitTools = pre-commit-hooks.outputs.packages.${system};
 
-      pre-commit-devShell = pkgs.mkShell {
-        name = "pre-commit-env";
-        inherit (pre-commit-check) shellHook;
-      };
+        pre-commit-devShell = pkgs.mkShell {
+          name = "pre-commit-env";
+          inherit (pre-commit-check) shellHook;
+        };
 
-      # Experimental env
-      experimentalDevShell = import ./experimental/build.nix {
-        inherit pkgs preCommitTools;
-        inherit (pre-commit-check) shellHook;
-      };
+        # Experimental env
+        experimentalDevShell = import ./experimental/build.nix {
+          inherit pkgs preCommitTools;
+          inherit (pre-commit-check) shellHook;
+        };
 
-      # Docs env
-      docsDevShell = import ./docs/build.nix {
-        inherit pkgs preCommitTools;
-        inherit (pre-commit-check) shellHook;
-      };
+        # Docs env
+        docsDevShell = import ./docs/build.nix {
+          inherit pkgs preCommitTools;
+          inherit (pre-commit-check) shellHook;
+        };
 
-      # Docs env
-      protosDevShell = import ./lambda-buffers-proto/build.nix {
+        # Protos env
+        pbnix-lib = protobufs-nix.lib.${system};
+
+        protosBuild = import ./lambda-buffers-proto/build.nix {
+          inherit pkgs pbnix-lib;
+          inherit (pre-commit-check) shellHook;
+        };
+
+        # Utilities
+        # INFO: Will need this; renameAttrs = rnFn: pkgs.lib.attrsets.mapAttrs' (n: value: { name = rnFn n; inherit value; });
+      in
+      rec {
+        # Useful for nix repl
         inherit pkgs;
-        inherit (pre-commit-check) shellHook;
-      };
 
-      # Utilities
-      # INFO: Will need this; renameAttrs = rnFn: pkgs.lib.attrsets.mapAttrs' (n: value: { name = rnFn n; inherit value; });
-    in
-    rec {
-      # Useful for nix repl
-      inherit pkgs;
+        # Standard flake attributes
+        packages = { inherit (protosBuild) compilerHsPb; };
 
-      # Instruction for the Hercules CI to build on x86_64-linux only, to avoid errors about systems without agents.
-      herculesCI.ciSystems = [ "x86_64-linux" ];
+        devShells = rec {
+          dev-pre-commit = pre-commit-devShell;
+          dev-experimental = experimentalDevShell;
+          dev-docs = docsDevShell;
+          dev-protos = protosBuild.protosDevShell;
+          default = pre-commit-devShell;
+        };
 
-      # Standard flake attributes
-      packages = { };
+        # nix flake check --impure --keep-going --allow-import-from-derivation
+        checks = { inherit pre-commit-check; } // devShells // packages;
 
-      devShells = rec {
-        dev-pre-commit = pre-commit-devShell;
-        dev-experimental = experimentalDevShell;
-        dev-docs = docsDevShell;
-        dev-protos = protosDevShell;
-        default = pre-commit-devShell;
-      };
-
-      # nix flake check --impure --keep-going --allow-import-from-derivation
-      checks = { inherit pre-commit-check; } // devShells // packages;
-
-    }
-  );
+      }
+    ) // {
+    # Instruction for the Hercules CI to build on x86_64-linux only, to avoid errors about systems without agents.
+    herculesCI.ciSystems = [ "x86_64-linux" ];
+  };
 }
--- lambda-buffers-proto/build.nix
+++ lambda-buffers-proto/build.nix
@@ -1,12 +1,22 @@
-{ pkgs, shellHook }:
-pkgs.mkShell {
-  name = "protos-env";
-  buildInputs = [
-    pkgs.protobuf
-    pkgs.protolint
-    pkgs.txtpbfmt
-    pkgs.haskellPackages.proto-lens-protoc
-  ];
+{ pkgs, pbnix-lib, shellHook }:
+{
+  protosDevShell = pkgs.mkShell {
+    name = "protos-env";
+    buildInputs = [
+      pkgs.protobuf
+      pkgs.protolint
+      pkgs.txtpbfmt
+      pkgs.haskellPackages.proto-lens-protoc
+    ];
 
-  inherit shellHook;
+    inherit shellHook;
+  };
+
+  compilerHsPb = pbnix-lib.haskellProto {
+    inherit pkgs;
+    src = ./.;
+    proto = "compiler.proto";
+    cabalPackageName = "lambda-buffers-compiler-pb";
+  };
+
 }
