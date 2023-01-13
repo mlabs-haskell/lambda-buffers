1e85f4a315f910eb1fd5330934d76647d07e578d
Drazen Popovic <bladyjoker@gmail.com>
2023-01-13 11:27:23 +0100

    Added good examples

--- lambda-buffers-frontend/resources/run.sh
+++ lambda-buffers-frontend/resources/run.sh
@@ -12,3 +12,5 @@ lbcli compile -i multiple_modules_found -i multiple_modules_found/another_import
 lbcli compile -i symbol_already_imported -f symbol_already_imported/A.lbf
 lbcli compile -i tydef_name_conflict -f tydef_name_conflict/A.lbf
 lbcli compile -i tyref_not_found -f tyref_not_found/A.lbf
+
+lbcli compile -i good -f good/Test.lbf
