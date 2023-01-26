b68766a5a913813d591d567b8110679f1470b0ef
Drazen Popovic <bladyjoker@gmail.com>
2023-01-26 18:14:13 +0100

    Renamed common into extras and fixed up everything

--- lambda-buffers-common/src/LambdaBuffers/Common/ProtoCompat.hs
+++
@@ -1,6 +0,0 @@
-module LambdaBuffers.Common.ProtoCompat (module X) where
-
-import LambdaBuffers.Common.ProtoCompat.IsMessage as X
-import LambdaBuffers.Common.ProtoCompat.NameLike as X
-import LambdaBuffers.Common.ProtoCompat.NameValidation as X
-import LambdaBuffers.Common.ProtoCompat.Types as X
