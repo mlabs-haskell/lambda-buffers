4f06a889618a3c203533bbb366565b3439b47dab
Vlad P. Luchian <cstmlcodes@gmail.com>
2023-02-17 12:32:25 +0000

    cleanup: remove redundant import

--- lambda-buffers-compiler/test/Test/KindCheck/Errors.hs
+++ lambda-buffers-compiler/test/Test/KindCheck/Errors.hs
@@ -2,7 +2,7 @@ module Test.KindCheck.Errors (testGKindCheckErrors) where
 
 import LambdaBuffers.Compiler.KindCheck (check_)
 import LambdaBuffers.Compiler.ProtoCompat qualified as PC
-import LambdaBuffers.Compiler.ProtoCompat.Types (CompilerError (CompKindCheckError), KindCheckError (UnboundTyRefError, UnboundTyVarError), Module)
+import LambdaBuffers.Compiler.ProtoCompat.Types (CompilerError (CompKindCheckError), KindCheckError (UnboundTyRefError, UnboundTyVarError))
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit (testCase, (@?=))
 import Test.Utils.CompilerInput (compilerInput'undefinedForeignTyRef, compilerInput'undefinedLocalTyRef, compilerInput'undefinedVariable)
