1ad20faee7df2885b1fb2f7e5e6a925ba4a28f70
Drazen Popovic <bladyjoker@gmail.com>
2023-02-23 16:13:28 +0100

    WIP haskell tydef printing

--- lambda-buffers-codegen/src/LambdaBuffers/Codegen/Haskell.hs
+++ lambda-buffers-codegen/src/LambdaBuffers/Codegen/Haskell.hs
@@ -1,15 +1,83 @@
-module LambdaBuffers.Codegen.Haskell (printSum) where
+module LambdaBuffers.Codegen.Haskell (printTyDef, printSum, CabalPackageName (..), HaskModuleName (..), HaskTyName (..)) where
 
+import Control.Lens (view, (^.))
+import Control.Monad.Error.Class (MonadError (throwError))
+import Control.Monad.RWS (MonadReader (local), MonadWriter (tell))
+import Control.Monad.RWS.Class (asks)
 import Data.Foldable (Foldable (toList))
-import LambdaBuffers.Compiler.ProtoCompat.Types (ConstrName (ConstrName), Constructor (Constructor), Field (Field), FieldName (FieldName), ForeignRef (ForeignRef), LocalRef (LocalRef), ModuleName (ModuleName), ModuleNamePart (ModuleNamePart), Product (RecordI, TupleI), Record (Record), Sum (Sum), Tuple (Tuple), Ty (TyAppI, TyRefI, TyVarI), TyApp (TyApp), TyName (TyName), TyRef (ForeignI, LocalI), TyVar (TyVar), VarName (VarName))
-import Prettyprinter (Doc, Pretty (pretty), align, colon, comma, concatWith, dot, encloseSep, group, lbrace, pipe, rbrace, sep, space, surround, (<+>))
+import Data.Generics.Labels ()
 
-printSum :: Sum -> Doc a
+-- TODO(bladyjoker): Remove this when possible
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Data.Text (Text)
+import LambdaBuffers.Compiler.ProtoCompat.Types (ConstrName (ConstrName), Constructor (Constructor), Field (Field), FieldName (FieldName), ForeignRef (ForeignRef), LocalRef (LocalRef), ModuleName (ModuleName), ModuleNamePart (ModuleNamePart), Product (RecordI, TupleI), Record (Record), Sum (Sum), Tuple (Tuple), Ty (TyAppI, TyRefI, TyVarI), TyAbs (TyAbs), TyApp (TyApp), TyArg (TyArg), TyBody (OpaqueI, SumI), TyDef, TyName (TyName), TyRef (ForeignI, LocalI), TyVar (TyVar), VarName (VarName))
+import Prettyprinter (Doc, Pretty (pretty), align, colon, comma, concatWith, dot, encloseSep, equals, group, lbrace, pipe, rbrace, sep, space, surround, (<+>))
+
+newtype CabalPackageName = MkCabalPackageName Text
+newtype HaskModuleName = MkHaskModuleName Text
+newtype HaskTyName = MkHaskTyName Text
+type QualifiedHaskTyRef = (CabalPackageName, HaskModuleName, HaskTyName)
+
+newtype PrintConfig = PrintConfig
+  { _opaques :: Map TyName QualifiedHaskTyRef
+  }
+
+newtype PrintCtx = TyDefCtx TyDef
+type PrintRead = (PrintConfig, PrintCtx)
+
+type PrintWrite = [PrintCommand]
+data PrintCommand = PrintTyDef (Doc ()) | AddImport QualifiedHaskTyRef | AddTyExport HaskTyName
+type PrintErr = String
+
+-- FIXME(bladyjoker): I need to keep the ordering on Constructors, Fields and TyArgs.
+
+askConfig :: MonadReader PrintRead m => m PrintConfig
+askConfig = asks fst
+
+askCtx :: MonadReader PrintRead m => m PrintCtx
+askCtx = asks snd
+
+askTyDef :: MonadReader PrintRead m => m TyDef
+askTyDef = do
+  ctx <- askCtx
+  case ctx of
+    TyDefCtx td -> return td
+
+printTyDef :: (MonadWriter PrintWrite m, MonadReader PrintRead m, MonadError PrintErr m) => TyDef -> m ()
+printTyDef td = do
+  local (\(cfg, _) -> (cfg, TyDefCtx td)) (printTyAbs $ td ^. #tyAbs)
+
+printTyAbs :: (MonadWriter PrintWrite m, MonadReader PrintRead m, MonadError PrintErr m) => TyAbs -> m ()
+printTyAbs (TyAbs _ (OpaqueI _) _) = do
+  PrintConfig cfg <- askConfig
+  tn <- view #tyName <$> askTyDef
+  qhsRef@(_, _, hsTyName) <- case Map.lookup tn cfg of
+    Nothing -> throwError $ "TODO(bladyjoker): Opaque not configured" <> show tn
+    Just qhsRef -> return qhsRef
+  tell
+    [ AddTyExport hsTyName
+    , AddImport qhsRef
+    ]
+printTyAbs (TyAbs args (SumI s) _) = do
+  sumDoc <- printSum s
+  td <- askTyDef
+  let tdDoc = group $ pretty (td ^. #tyName . #name) <+> sep (printTyArg <$> toList args) <+> equals <+> sumDoc
+  tell
+    [ AddTyExport (MkHaskTyName $ td ^. #tyName . #name)
+    , PrintTyDef tdDoc
+    ]
+
+printTyArg :: forall {a}. TyArg -> Doc a
+printTyArg (TyArg vn _ _) = printVarName vn
+
+printSum :: Monad m => Sum -> m (Doc ())
 printSum (Sum ctors _) =
-  group $
-    if null ctors
-      then mempty
-      else align $ encloseSep mempty mempty (space <> pipe <> space) (printCtor <$> toList ctors)
+  return $
+    group $
+      if null ctors
+        then mempty
+        else align $ encloseSep mempty mempty (space <> pipe <> space) (printCtor <$> toList ctors)
 
 printCtor :: Constructor -> Doc a
 printCtor (Constructor ctorName prod) = align $ group (printCtorName ctorName <> printProd prod)
