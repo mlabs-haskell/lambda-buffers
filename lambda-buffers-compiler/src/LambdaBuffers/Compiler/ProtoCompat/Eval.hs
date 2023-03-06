b8756d1c8bbb8be1edb98838f5b2a4fba2272b04
Drazen Popovic <bladyjoker@gmail.com>
2023-03-06 19:32:53 +0100

    Lensifies Context and throws P.CompilerError

--- lambda-buffers-compiler/src/LambdaBuffers/Compiler/ProtoCompat/Eval.hs
+++ lambda-buffers-compiler/src/LambdaBuffers/Compiler/ProtoCompat/Eval.hs
@@ -1,16 +1,17 @@
 module LambdaBuffers.Compiler.ProtoCompat.Eval (Ty (..), fromTy, eval) where
 
-import Control.Lens (view, (^.))
+import Control.Lens (makeLenses, view, (%~), (&), (.~), (^.))
 import Control.Monad.Error.Class (MonadError (throwError))
-import Control.Monad.Reader.Class (MonadReader (ask, local))
+import Control.Monad.Reader.Class (MonadReader (local), asks)
 import Data.Foldable (Foldable (toList))
 import Data.Map (Map)
 import Data.Map qualified as Map
 import Data.Map.Ordered (OMap)
 import Data.Map.Ordered qualified as OMap
-import Data.Text (Text)
-import Data.Text qualified as Text
+import Data.ProtoLens (Message (defMessage))
 import LambdaBuffers.Compiler.ProtoCompat qualified as PC
+import Proto.Compiler qualified as P
+import Proto.Compiler_Fields qualified as P
 
 data Ty
   = TyApp Ty [Ty] PC.TyApp
@@ -21,6 +22,7 @@ data Ty
   | TySum (OMap (PC.InfoLess PC.ConstrName) Ty) PC.Sum
   | TyTuple [Ty] PC.Product
   | TyRecord (OMap (PC.InfoLess PC.FieldName) Ty) PC.Product
+  deriving stock (Eq, Ord, Show)
 
 fromTy :: PC.Ty -> Ty
 fromTy (PC.TyVarI tv) = TyVar tv
@@ -42,42 +44,72 @@ fromTyProd p@(PC.RecordI (PC.Record fields _)) = TyRecord (fromTyField <$> field
   where
     fromTyField (PC.Field _fn ty) = fromTy ty
 
-type Context = (PC.ModuleName, Map PC.QTyName PC.TyDef, Map (PC.InfoLess PC.VarName) Ty)
+data Context = MkContext
+  { _ctxModuleName :: PC.ModuleName
+  , _ctxTyDefs :: Map PC.QTyName PC.TyDef
+  , _ctxTyArgs :: Map (PC.InfoLess PC.VarName) Ty
+  }
+  deriving stock (Eq, Ord, Show)
 
-type MonadEval m = (MonadError Text m, MonadReader Context m)
+makeLenses 'MkContext
+
+type MonadEval m = (MonadError P.CompilerError m, MonadReader Context m)
 
 eval :: MonadEval m => Ty -> m Ty
 eval (TyRef tr) = resolveTyRef tr >>= eval
 eval (TyApp (TyAbs args' body _) tys _) =
   let actx' = Map.fromList $ zip (PC.mkInfoLess . view #argName <$> toList args') tys
-   in local (\(mn, tds, actx) -> (mn, tds, Map.union actx' actx)) (subst body)
+   in local (\ctx -> ctx & ctxTyArgs %~ Map.union actx') (subst body)
 eval t = return t
 
 resolveTyRef :: MonadEval m => PC.TyRef -> m Ty
-resolveTyRef (PC.LocalI tr@(PC.LocalRef tn _si)) = do
-  (mn, tds, _args) <- ask
+resolveTyRef tr@(PC.LocalI (PC.LocalRef tn _si)) = do
+  mn <- asks (view ctxModuleName)
+  tds <- asks (view ctxTyDefs)
   case Map.lookup (PC.mkInfoLess mn, PC.mkInfoLess tn) tds of
-    Nothing -> throwError ("TODO(bladyjoker): UnboundTyRef " <> (Text.pack . show $ tr))
+    Nothing ->
+      throwError $
+        defMessage
+          & P.kindCheckErrors -- FIXME(bladyjoker): Parametrize error messages on 'stage' (KindChecking being one of them).
+            .~ [ defMessage
+                  & P.unboundTyRefError . P.moduleName .~ PC.toProto mn
+                  & P.unboundTyRefError . P.tyRef .~ PC.toProto tr
+               ]
     Just td -> return $ fromTyAbs (td ^. #tyAbs)
-resolveTyRef (PC.ForeignI tr@(PC.ForeignRef tn mn _si)) = do
-  (_mn, tds, _args) <- ask
+resolveTyRef tr@(PC.ForeignI (PC.ForeignRef tn mn _si)) = do
+  tds <- asks (view ctxTyDefs)
   case Map.lookup (PC.mkInfoLess mn, PC.mkInfoLess tn) tds of
-    Nothing -> throwError ("TODO(bladyjoker): UnboundTyRef " <> (Text.pack . show $ tr))
+    Nothing ->
+      throwError $
+        defMessage
+          & P.kindCheckErrors -- FIXME(bladyjoker): Parametrize error messages on 'stage' (KindChecking being one of them).
+            .~ [ defMessage
+                  & P.unboundTyRefError . P.moduleName .~ PC.toProto mn
+                  & P.unboundTyRefError . P.tyRef .~ PC.toProto tr
+               ]
     Just td -> return $ fromTyAbs (td ^. #tyAbs)
 
 resolveTyVar :: MonadEval m => PC.TyVar -> m Ty
 resolveTyVar tv = do
-  (_mn, _td, args) <- ask
+  args <- asks (view ctxTyArgs)
   case Map.lookup (PC.mkInfoLess $ tv ^. #varName) args of
-    Nothing -> throwError $ "TODO(bladyjoker): UnboundTyVar " <> (Text.pack . show $ tv)
+    Nothing -> do
+      mn <- asks (view ctxModuleName)
+      throwError $
+        defMessage
+          & P.kindCheckErrors -- FIXME(bladyjoker): Parametrize error messages on 'stage' (KindChecking being one of them).
+            .~ [ defMessage
+                  & P.unboundTyVarError . P.moduleName .~ PC.toProto mn
+                  & P.unboundTyVarError . P.tyVar .~ PC.toProto tv
+               ]
     Just ty -> return ty
 
 subst :: MonadEval m => Ty -> m Ty
 subst (TyVar tv) = resolveTyVar tv
 subst (TyAbs args' body _) = do
-  (mn, tds, args) <- ask
-  let x = Map.difference args (OMap.toMap args')
-  local (const (mn, tds, x)) (subst body)
+  args <- asks (view ctxTyArgs)
+  let unbound = Map.difference args (OMap.toMap args')
+  local (\ctx -> ctx & ctxTyArgs .~ unbound) (subst body)
 subst (TyApp f args t) = TyApp <$> subst f <*> (subst `traverse` args) <*> pure t
 subst (TySum ctors s) = TySum <$> (subst `traverse` ctors) <*> pure s
 subst (TyTuple fields t) = TyTuple <$> (subst `traverse` fields) <*> pure t
