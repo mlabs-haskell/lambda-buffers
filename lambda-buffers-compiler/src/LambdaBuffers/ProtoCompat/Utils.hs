2c18f455c2c68cd93cb5494d9ec175f5f1e26edf
Drazen Popovic <bladyjoker@gmail.com>
2023-05-25 16:44:05 +0200

    Compiler error printing

--- lambda-buffers-compiler/src/LambdaBuffers/ProtoCompat/Utils.hs
+++ lambda-buffers-compiler/src/LambdaBuffers/ProtoCompat/Utils.hs
@@ -1,18 +1,25 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
 {-# OPTIONS_GHC -Wno-redundant-constraints #-}
 
-module LambdaBuffers.ProtoCompat.Utils (prettyModuleName, localRef2ForeignRef) where
+module LambdaBuffers.ProtoCompat.Utils (prettyModuleName, prettyModuleName', localRef2ForeignRef) where
 
 import Control.Lens (Getter, to, view, (&), (.~), (^.))
 import Data.ProtoLens (Message (defMessage))
+import LambdaBuffers.ProtoCompat.IsCompat.FromProto qualified as PC
+import LambdaBuffers.ProtoCompat.IsCompat.Lang ()
 import LambdaBuffers.ProtoCompat.Types qualified as PC
 import Prettyprinter (Doc, Pretty (pretty), dot, encloseSep)
 import Proto.Codegen qualified as Codegen
 import Proto.Compiler qualified as Compiler
 import Proto.Compiler_Fields qualified as Compiler
+import Proto.Lang qualified as Lang
+import Proto.Lang_Fields qualified as Lang
 
 prettyModuleName :: PC.ModuleName -> Doc a
-prettyModuleName mn = encloseSep mempty mempty dot $ pretty . view #name <$> mn ^. #parts
+prettyModuleName mn = prettyModuleName' $ PC.toProto mn -- IsCompat instances are in PC.Lang
+
+prettyModuleName' :: Lang.ModuleName -> Doc a
+prettyModuleName' mn = encloseSep mempty mempty dot $ pretty . view Lang.name <$> mn ^. Lang.parts
 
 localRef2ForeignRef :: PC.ModuleName -> Getter PC.LocalRef PC.ForeignRef
 localRef2ForeignRef modName =
