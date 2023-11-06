4d603d3868868518826dafe41ffbf4e8a4d9cf3e
Szabo Gergely <gege251@mailbox.org>
2023-11-06 17:52:12 +0100

    Solving Rust local dependencies

--- runtimes/rust/lbr-prelude/build.nix
+++ runtimes/rust/lbr-prelude/build.nix
@@ -1,36 +1,67 @@
 { inputs, ... }: {
   perSystem = { self', pkgs, system, ... }:
     let
-      crateName = "lbr-prelude";
-      rustWithTools = pkgs.rust-bin.stable.latest.default.override {
-        extensions = [ "rustfmt" "rust-analyzer" "clippy" ];
-      };
-      craneLib = inputs.crane.lib.${system}.overrideToolchain rustWithTools;
-      src = craneLib.cleanCargoSource (craneLib.path ./.);
-      commonArgs = {
-        inherit src;
-        strictDeps = true;
-      };
-      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+      rustProject = { crateName, localDeps, dataDeps }:
+        let
+          rustWithTools = pkgs.rust-bin.stable.latest.default.override {
+            extensions = [ "rustfmt" "rust-analyzer" "clippy" ];
+          };
+          craneLib = inputs.crane.lib.${system}.overrideToolchain rustWithTools;
 
+          buildEnv = pkgs.stdenv.mkDerivation {
+            src = ./.;
+            name = "lbf-rust-workspace";
+            unpackPhase = builtins.concatStringsSep "\n"
+              ([
+                "mkdir -p $out"
+                "cp -r $src $out/${crateName}"
+              ]
+              ++ (map ({ name, path }: "cp -r ${path} $out/${name}") localDeps)
+              ++ (map ({ name, path }: "cp -r ${path} $out/${name}") dataDeps)
+              );
+          };
+          commonArgs = {
+            src = buildEnv;
+            pname = crateName;
+            strictDeps = true;
+            cargoLock = ./Cargo.lock;
+            cargoToml = ./Cargo.toml;
+            postUnpack = ''
+              cd $sourceRoot/${crateName}
+              sourceRoot="."
+            '';
+          };
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        in
+        {
+          devShells."dev-${crateName}-rust" = craneLib.devShell {
+            checks = self'.checks;
+          };
+
+          packages."${crateName}-rust" = craneLib.buildPackage commonArgs // {
+            inherit cargoArtifacts;
+            doCheck = false;
+          };
+
+          checks."${crateName}-rust-test" = craneLib.cargoNextest (commonArgs // {
+            inherit cargoArtifacts;
+          });
+
+          checks."${crateName}-rust-clippy" = craneLib.cargoClippy (commonArgs // {
+            inherit cargoArtifacts;
+          });
+        };
     in
-    {
-      devShells."dev-${crateName}-rust" = craneLib.devShell {
-        checks = self'.checks;
-      };
-
-      packages."${crateName}-rust" = craneLib.buildPackage (commonArgs // {
-        inherit cargoArtifacts;
-        doTest = false;
-      });
-
-      checks."${crateName}-rust-test" = craneLib.cargoNextest (commonArgs // {
-        inherit cargoArtifacts;
-      });
-
-      checks."${crateName}-rust-clippy" = craneLib.cargoClippy (commonArgs // {
-        inherit cargoArtifacts;
-      });
+    rustProject {
+      crateName = "lbr-prelude";
+      localDeps = [
+        {
+          name = "lbr-prelude-derive";
+          path = ../lbr-prelude-derive;
+        }
+      ];
+      dataDeps = [ ];
     };
 
 }
