55e9184754cd66944be83c2b5988b8b76b772ff1
t4ccer <t4ccer@gmail.com>
2023-11-17 03:09:33 -0700

    Add simple Emacs mode
    Simple mode derived from Haskell mode to not write custom parsers in this rather
    early stage. Indentation is a bit janky but you can hit tab couple times to get
    what you mean. Syntax highlighting works just fine.

---
+++ editor/emacs/lambda-buffers-mode.el
@@ -0,0 +1,16 @@
+;;; lambda-buffers-mode.el --- SUMMARY -*- lexical-binding: t -*-
+
+(require 'haskell-mode)
+
+(define-derived-mode lambda-buffers-mode haskell-mode "lambda-buffers"
+  "haskell-mode pretending to be lambda-buffers-mode"
+  (haskell-indent-mode)
+  (font-lock-add-keywords
+   nil
+   '(("\\<\\(record\\|sum\\|derive\\)\\>" . 'font-lock-keyword-face)))
+  (setq lsp-warn-no-matched-clients nil))
+
+(add-to-list 'auto-mode-alist '("\\.lbf$" . lambda-buffers-mode))
+
+(provide 'lambda-buffers-mode)
+;;; lambda-buffers-mode.el ends here
