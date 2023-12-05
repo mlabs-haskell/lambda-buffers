3a3f0acd55340ff17584f52b5193fbcd11469e97
jared <>
2023-12-04 17:04:38 -0700

    Added nix script for lbf-plutus-to-typescript

--- lambda-buffers-frontend/build.nix
+++ lambda-buffers-frontend/build.nix
@@ -163,6 +163,24 @@ _:
           }} "$@";
         '';
 
+        lbf-plutus-to-typescript = pkgs.writeShellScriptBin "lbf-plutus-to-typescript" ''
+          export LB_COMPILER=${config.packages.lbc}/bin/lbc;
+          mkdir -p autogen;
+          mkdir -p .work;
+          ${config.lbf-nix.lbfBuild.buildCall {
+            files = [];
+            import-paths = [ config.packages.lbf-prelude config.packages.lbf-plutus ];
+            gen = "${config.packages.lbg-typescript}/bin/lbg-typescript";
+            gen-classes = [ "Prelude.Eq" "Prelude.Json" "Plutus.V1.PlutusData" ];
+            gen-dir = "autogen";
+            gen-opts = [
+              "--config=${config.packages.codegen-configs}/typescript-prelude-base.json"
+              "--config=${config.packages.codegen-configs}/typescript-plutus.json"
+            ];
+            work-dir = ".work";
+          }} "$@";
+        '';
+
       };
 
 
