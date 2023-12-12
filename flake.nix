b4b16f928015051b3b12a3da1f147f778c802425
Drazen Popovic <bladyjoker@gmail.com>
2023-12-12 13:21:15 +0100

    nix flake lock --update-input ctl

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
     # Purescript
 
     ## Cardano transaction library (leveraging CTL's Purescript Nix machinery)
-    ctl.url = "github:plutonomicon/cardano-transaction-lib?rev=49766d535b1fdb58ab4311f718a6f65024772377";
+    ctl.url = "github:plutonomicon/cardano-transaction-lib?ref=dshuiski/purs-0.15-migration";
     ctl-nixpkgs.follows = "ctl/nixpkgs";
 
     # Rust
