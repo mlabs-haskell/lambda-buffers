e00a80bc29c03130c5839b6de20652fd619faac3
Szabo Gergely <gege251@mailbox.org>
2023-12-20 15:51:45 +0100

    lbt-plutus-rust: Tempararily add git ref for pla-rust

--- extras/lbf-nix/lbf-rust.nix
+++ extras/lbf-nix/lbf-rust.nix
@@ -78,13 +78,12 @@ let
       '';
     };
 
-  # 
   crateVersions = pkgs.writeTextFile {
     name = "lambda-buffers-crate-versions";
     text = ''
       num-bigint = "0.4.4"
       serde_json = { version = "1.0.107", features = ["arbitrary_precision"] }
-      plutus-ledger-api = { git = "https://github.com/mlabs-haskell/plutus-ledger-api-rust", features = [ "lbf", ], rev = "c9e09c867decdcb6e405b1231dad447436c820c5" }
+      plutus-ledger-api = { git = "https://github.com/mlabs-haskell/plutus-ledger-api-rust", features = [ "lbf", ], ref = "szg251/fix-lbf", rev = "4460f2ff8af7993a24209ebe28df5ef9b3b3b397" }
     '';
   };
 
