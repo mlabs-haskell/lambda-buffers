6e03b6767276c3f734a33744fffbe04beaa5aca0
jared <>
2024-01-11 20:03:47 -0700

    Use flake-lang.nix for typescript lbr-prelude

--- runtimes/typescript/lbr-prelude/build.nix
+++ runtimes/typescript/lbr-prelude/build.nix
@@ -1,24 +1,25 @@
-{ ... }:
+{ inputs, ... }:
 {
-  perSystem = { inputs', config, ... }:
+  perSystem = { system, inputs', config, ... }:
     let
       typescriptFlake =
-        config.lbf-nix.typescriptFlake {
+        inputs.flake-lang.lib.${system}.typescriptFlake {
           name = "lbr-prelude";
           src = ./.;
 
           devShellTools = config.settings.shell.tools;
           devShellHook = config.settings.shell.hook;
 
-          npmDependencies = [ inputs'.prelude-typescript.packages.tgz ];
+          npmExtraDependencies = [ inputs'.prelude-typescript.packages.tgz ];
         };
     in
     {
       packages = {
-        lbr-prelude-typescript = typescriptFlake.packages.lbr-prelude-typescript;
-        lbr-prelude-typescript-tgz = typescriptFlake.packages.lbr-prelude-typescript-tgz;
-        lbr-prelude-typescript-nix-npm-folder-dependencies = typescriptFlake.packages.lbr-prelude-typescript-nix-npm-folder-dependencies;
-        lbr-prelude-typescript-node2nix = typescriptFlake.packages.lbr-prelude-typescript-node2nix;
+        inherit (typescriptFlake.packages) 
+            lbr-prelude-typescript 
+            lbr-prelude-typescript-tgz 
+            lbr-prelude-typescript-nix-npm-folder-dependencies 
+            lbr-prelude-typescript-node2nix;
       };
 
       inherit (typescriptFlake) checks devShells;
