a08502f50819c4b4d63fc37dfdf0262a22741e11
Drazen Popovic <bladyjoker@gmail.com>
2024-07-10 18:15:30 +0200

    Haskell codegen uses collectVars from the compiler

--- lambda-buffers-codegen/src/LambdaBuffers/Codegen/Haskell/Print/InstanceDef.hs
+++ lambda-buffers-codegen/src/LambdaBuffers/Codegen/Haskell/Print/InstanceDef.hs
@@ -1,9 +1,5 @@
-module LambdaBuffers.Codegen.Haskell.Print.InstanceDef (printInstanceDef, printConstraint, collectTyVars, printInstanceContext, printInstanceContext', printConstraint') where
+module LambdaBuffers.Codegen.Haskell.Print.InstanceDef (printInstanceDef, printConstraint, printInstanceContext, printInstanceContext', printConstraint') where
 
-import Control.Lens (view)
-import Data.Foldable (Foldable (toList))
-import Data.Set (Set)
-import Data.Set qualified as Set
 import LambdaBuffers.Codegen.Haskell.Backend (MonadHaskellBackend)
 import LambdaBuffers.Codegen.Haskell.Print.Syntax qualified as HsSyntax
 import LambdaBuffers.Codegen.Haskell.Print.TyDef (printTyInner)
@@ -23,7 +19,7 @@ instance (SomeClass a, SomeClass b, SomeClass c) => SomeClass (SomeTy a b c) whe
 -}
 printInstanceDef :: forall t m ann. MonadHaskellBackend t m => HsSyntax.QClassName -> PC.Ty -> m (Doc ann -> m (Doc ann))
 printInstanceDef hsQClassName ty = do
-  let freeVars = collectTyVars ty
+  let freeVars = PC.collectTyVars ty
   headDoc <- printConstraint hsQClassName ty
   return $ case freeVars of
     [] -> \implDoc -> return $ "instance" <+> headDoc <+> "where" <> hardline <> space <> space <> implDoc
@@ -47,14 +43,3 @@ printConstraint' qcn tys = do
   let crefDoc = HsSyntax.printHsQClassName qcn
   tyDocs <- traverse printTyInner tys
   return $ crefDoc <+> hsep tyDocs
-
-collectTyVars :: PC.Ty -> [PC.Ty]
-collectTyVars = fmap (`PC.withInfoLess` (PC.TyVarI . PC.TyVar)) . toList . collectVars
-
-collectVars :: PC.Ty -> Set (PC.InfoLess PC.VarName)
-collectVars = collectVars' mempty
-
-collectVars' :: Set (PC.InfoLess PC.VarName) -> PC.Ty -> Set (PC.InfoLess PC.VarName)
-collectVars' collected (PC.TyVarI tv) = Set.insert (PC.mkInfoLess . view #varName $ tv) collected
-collectVars' collected (PC.TyAppI (PC.TyApp _ args _)) = collected `Set.union` (Set.unions . fmap collectVars $ args)
-collectVars' collected _ = collected
