5a952897c50405e988c3e512fada953c330da9a3
Szabo Gergely <gege251@mailbox.org>
2024-11-20 19:47:14 +0100

    wip

--- settings.nix
+++ settings.nix
@@ -43,8 +43,8 @@
           settings = {
 
             haskell = {
-              index-state = "2024-01-16T11:00:00Z";
-              compiler-nix-name = "ghc963";
+              index-state = "2024-10-16T00:00:00Z";
+              compiler-nix-name = "ghc966";
             };
 
             shell = {
