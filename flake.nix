97b5b4637282fac465f5bc6fcef1d01764f194ef
Szabo Gergely <gege251@mailbox.org>
2025-01-27 14:20:03 +0100

    Add aarc64 to supported systems

--- flake.nix
+++ flake.nix
@@ -79,6 +79,6 @@
         ./docs/typescript-plutus/build.nix
       ];
       debug = true;
-      systems = [ "x86_64-linux" "x86_64-darwin" ];
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux" ];
     };
 }
