dfe68e4834625c4f9c4a7db35b819778f9dc4f44
Szabo Gergely <gege251@mailbox.org>
2025-08-13 17:56:45 +0900

    Revert to previous Plutarch version

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     };
 
     # Plutarch eDSL (LB Codegen target)
-    plutarch.url = "github:Plutonomicon/plutarch-plutus?ref=75c06ef1e77742916574414975f76ddac59cce4a";
+    plutarch.url = "github:Plutonomicon/plutarch-plutus?ref=e9e9df286768440733890b1260ad569a2f882890";
 
     # Typescript runtimes
     prelude-typescript.follows = "plutus-ledger-api-typescript/prelude-typescript";
